9cef4d3d4fb3d00c68cd8c67ecd1d9a8e3190148
K4mp3rX1d <141189533+K4mp3rX1d@users.noreply.github.com>
2024-04-24 04:44:59 +0600

    Add functionality to show values in display

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ const keypad = document.querySelector('.keypad');
 
 // Function declarations:
 function drawKeys() {
-    keys = ['C', '%', 'rt', '<', 1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, 'x', '.', 0, '=', '/'];
+    keys = ['C', '%', '√', '⌫', 1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, 'x', '.', 0, '=', '/'];
     for (const key of keys) {
         const button = document.createElement('button');
         button.textContent = key;
-        button.classList.add('key')
+        button.classList.add('key');    
+        button.addEventListener('click', printKey);
         keypad.appendChild(button);
     }
+}
+
+function printKey(event) {
+    const key = event.target.textContent;
+    display.textContent += key
 }
 
 drawKeys();
